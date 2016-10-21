389012d465b6f14b676f0230923e9c35586b1b76
Laurent Fazio <laurent.fazio@dev-at-home.fr>
2016-10-21 14:14:10 +0200

    mgt.sh: Add remote management at mgt init.

--- bin/mgt.sh
+++ bin/mgt.sh
@@ -3,7 +3,7 @@
 PATH=$PATH:$(dirname $0)
 
 usage () {
-    echo "usage: mgt init <-h|--help> -r <remote>"
+    echo "usage: mgt init <-h|--help> --new -r <remote>"
     echo "       mgt project <-h|--help|init|list|select|sync> ..."
     echo "       mgt task <-h|--help|create|move|edit||comment|tag|rm|filter> ..."
     echo "       mgt category <-h|--help|create|edit|rm|filter> ..."
@@ -21,67 +21,79 @@ case $1 in
             ### TODO: Validate arguments
             case $1 in
                 -r|--remote)
-                remote=$2
-                shift #consume -r
-                shift #consume remote
-                ;;
+                    remote=$2
+                    shift #consume -r
+                    shift #consume remote
+                    ;;
+		        -n|--new)
+		            new=yes
+		            ;;
                 *)
-                echo "mgt: unknown option '$1'"
-                break
-                ;;
+                    echo "mgt: unknown option '$1'"
+                    break
+                    ;;
             esac
             shift
         done
 
-	touch ~/.mgtconfig
-	echo '### Do not modify' >> ~/.mgtconfig
-	echo '### Variables' >> ~/.mgtconfig
-	echo 'MGT_PATH=~/.mgt' >> ~/.mgtconfig
-	echo 'MGT_CONF_PATH=$MGT_PATH/conf.d' >> ~/.mgtconfig
-	echo 'MGT_PROJECT_PATH=$MGT_PATH/project' >> ~/.mgtconfig
-	echo '### Commands' >> ~/.mgtconfig
-	echo 'GIT="git --work-tree=$MGT_PATH --git-dir=$MGT_PATH/.git"' >> ~/.mgtconfig
+	    touch ~/.mgtconfig
+	    echo '### Do not modify' >> ~/.mgtconfig
+	    echo '### Variables' >> ~/.mgtconfig
+	    echo 'MGT_PATH=~/.mgt' >> ~/.mgtconfig
+	    echo 'MGT_CONF_PATH=$MGT_PATH/conf.d' >> ~/.mgtconfig
+	    echo 'MGT_PROJECT_PATH=$MGT_PATH/project' >> ~/.mgtconfig
+	    echo '### Commands' >> ~/.mgtconfig
+	    echo 'GIT="git --work-tree=$MGT_PATH --git-dir=$MGT_PATH/.git"' >> ~/.mgtconfig
 
-	. ~/.mgtconfig
+	    . ~/.mgtconfig
 
-	echo " * Create $MGT_PATH and subdirectories..."
-        mkdir -p $MGT_PATH/conf.d
-        mkdir -p $MGT_PATH/project
-	echo "Tutorial mgt" > $MGT_PATH/README
-        $GIT init
-        echo "mgt: project management" > $MGT_PATH/.git/description
-        $GIT add .
-        $GIT commit -s -m "Project: create project management repository"
-        if [ ! -z "$remote" ]; then
-	    echo " * Use remote $remote"
-            $GIT remote remove origin
-            $GIT remote add origin $remote
-            $GIT push origin master
-        fi
-    ;;
+	    if [ -z "$new" ]; then
+            mkdir -p $MGT_PATH/conf.d
+            mkdir -p $MGT_PATH/project
+	        echo "mgt repository" > $MGT_PATH/README
+            $GIT init
+            echo "mgt: project management" > $MGT_PATH/.git/description
+            $GIT add .
+            $GIT commit -s -m "Project: create project management repository"
+            if [ ! -z "$remote" ]; then
+		        $GIT remote remove origin
+		        $GIT remote add origin $remote
+		        $GIT push origin master
+            fi
+	    else
+            if [ ! -z "$remote" ]; then
+	            git clone $remote ~/.mgt
+            else
+                echo " *** You specified a not new remote: --remote is then mandatory..."
+                echo ""
+                usage
+                exit 1
+            fi
+	    fi
+	    ;;
     project)
         shift
         mgt-project.sh $@
-    ;;
+	    ;;
     task)
         shift
         mgt-task.sh $@
-    ;;
+	    ;;
     category)
         shift
         mgt-category.sh $@
-    ;;
+	    ;;
     config)
         shift
         mgt-config.sh $@
-    ;;
+	    ;;
     -h|--help)
         usage
-    ;;
+        ;;
     *)
         usage
         exit 1
-    ;;
+        ;;
 esac
 
 exit $?
