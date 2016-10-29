5804475496db3d3efd7bc40ef83928f68b09dcb7
Jean-François Morcillo <jfm@laposte.net>
2016-10-29 22:07:51 +0200

    test_mgt_init.sh: rework testing mechanism.

--- tests/test_mgt_init.sh
+++ tests/test_mgt_init.sh
@@ -9,19 +9,73 @@ fi
 sed -i -e 's#MGT_PATH=~/.mgt.*#MGT_PATH=~/.mgt-test#' ~/.mgtconfig
 . ~/.mgtconfig
 
+# check_res "test_name" "expected result" "result" "command output"
+check_res () {
+    echo "$4" > "$1".out
+    
+#    diff -q "$1".out "$1".out.ref
+    
+    if { [ "$2" == "ok" ] && [ ! "$3" == "0" ]; } ||
+       { [ "$2" == "nok" ] && [ "$3" == "0" ]; } ; then
+        # ERROR
 
+        while [ true ]; do
+            echo ">> ERROR running test \"$1\""
+            echo "received $3 while $2 was awaited"
+            echo "sh(o)w output, (s)how diff, (u)pdate ref, (n)ext"
+            read input
+            case $input in
+                o)
+                    cat "$1".out
+                    ;;
+                s|show)
+                    diff "$1".out "$1".out.ref
+                    ;;
+                u)
+                    cp "$1".out "$1".out.ref
+                    ;;
+                ""|n|next)
+                    break
+                    ;;
+                *)
+                    ;;
+            esac
+        done
 
+    else
+        echo "test \"$1\" ok"
+    fi
+}
+
+#do_test [-i] "test_name" "expected result: ok|nok" "test command" ["command sequence"]
 do_test () {
+    interactive=''
     if [ "$1" == "-i" ]; then
         shift
+        interactive=true
     fi
-    echo -n "##### $1"
+    test_name="$1"
+    shift
+    exp_res="$1"
+    shift
+    
+    echo -n "##### test $test_name : $1"
     if [ $# -eq 1 ]; then
         echo
-        $1
+        if [ ! $interactive ]; then
+            test_out=$($1 2>&1)
+            check_res "$test_name" "$exp_res" "$?" "$test_out"
+        else
+            $1
+        fi
     else
         echo $2
-        $1 <<< "$2"
+        if [ ! $interactive ]; then
+            test_out=$($1 <<< "$2" 2>&1)
+            check_res "$test_name" "$exp_res" "$?" "$test_out"
+        else
+            $1 <<< "$2"
+        fi
     fi
     echo
 }
@@ -34,61 +88,53 @@ echo "##################################################"
 echo "Press enter to continue or C^c to quit"
 read
 
-do_test "rm -rf $MGT_PATH"
 rm -rf $MGT_PATH
+do_test "1" "ok" "mgt init -r https://github.com/bilboquet/test.git"
 
-do_test "mgt init -r https://github.com/bilboquet/test.git"
-
-do_test "rm -rf $MGT_PATH"
 rm -rf $MGT_PATH
+do_test "2" "nok" "mgt init"
 
-do_test "mgt init"
+do_test "3" "ok" "mgt init --new"
 
-do_test "mgt init --new"
-
-do_test "rm -rf $MGT_PATH"
 rm -rf $MGT_PATH
+do_test "4" "ok" "mgt init --new -r https://github.com/bilboquet/test.git"
 
-do_test "mgt init --new -r https://github.com/bilboquet/test.git"
-
-do_test "rm -rf $MGT_PATH"
 rm -rf $MGT_PATH
+do_test "5" "ok" "mgt init -n -r https://github.com/bilboquet/test.git --force"
 
-do_test "mgt init -n -r https://github.com/bilboquet/test.git --force"
+do_test "6" "nok" "mgt project init"
 
-do_test "mgt project init"
+do_test "7" "ok" "mgt project init test_proj1"
 
-do_test "mgt project init test_proj1"
+do_test "7.1" "nok" "mgt project init test_proj1"
 
-do_test "mgt project init test_proj1"
+do_test "7.2" "ok" "mgt project init test_proj2"
 
-do_test "mgt project init test_proj2"
+do_test "8" "ok" "mgt project list"
 
-do_test "mgt project list"
+do_test "9" "ok" "mgt task search"
 
-do_test "mgt task list"
+do_test "9.1" "nok" "mgt task add"
 
-do_test "mgt task create"
+do_test "9.2" "nok" "mgt task add -c todo"
 
-do_test "mgt task create -c todo"
+do_test "-i" "9.3" "ok" "mgt task add -c todo -d description"
 
-do_test "mgt task create -c todo -d description"
-
-do_test "mgt task list"
+do_test "9.4" "ok" "mgt task search"
 
 seq=$'s\nh\na\nh\nq'
-do_test "mgt task list -i" "$seq"
+do_test "9.5" "ok" "mgt task search -i" "$seq"
 
-do_test "mgt task list -f Assignee=Jean"
+do_test "9.6" "ok" "mgt task search -f Assignee=Jean"
 
-do_test "mgt project sync"
+do_test "9.7" "ok" "mgt project sync"
 
-do_test "mgt project select test_proj1"
+do_test "9.8" "ok" "mgt project select test_proj1"
 
-do_test "mgt task create -c todo -d description"
+do_test "-i" "9.9" "ok" "mgt task add -c todo -d description"
 
-do_test "mgt project sync"
+do_test "9.10" "ok" "mgt project sync"
 
 
-### end of tests
-sed -i -e 's#MGT_PATH=~/.mgt-test.*#MGT_PATH=~/.mgt#' ~/.mgtconfig
+#### end of tests
+#sed -i -e 's#MGT_PATH=~/.mgt-test.*#MGT_PATH=~/.mgt#' ~/.mgtconfig
