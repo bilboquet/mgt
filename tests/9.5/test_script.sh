2613e1385462bd2406a04304817c1cb28314a48a
Jean-François Morcillo <jfm@laposte.net>
2016-11-09 10:36:22 +0100

    tests/9.5: remove useless out.ref, add check_test.
    Signed-off-by: Jean-François Morcillo <jfm@laposte.net>

--- tests/9.5/test_script.sh
+++ tests/9.5/test_script.sh
@@ -14,4 +14,20 @@ INPUTS=$'s\nh\na\nh\nq'
 
 [[ "$1" == "setup" ]] && setup
 
+cat > "check_test" << 'EOL'
+#!/bin/bash
+. ~/.mgtconfig
+set -x
+
+nb_task=$(ls "$MGT_PROJECT_PATH/todo" | wc -l)
+[[ $nb_task -eq 1 ]] || exit 1 # wrong number of tasks
+# get first task
+first_task=$(ls "$MGT_PROJECT_PATH/todo" | head -n1)
+# check that 1st task was "self assigned" during test
+grep -e "Assignee: $(git config user.name) <$(git config user.email)>" "$MGT_PROJECT_PATH/todo/$first_task"
+[[ $? -eq 0 ]] || exit 1 # task was not correctly assigned during test
+
+exit 0
+EOL
+
 mgt task search -i
